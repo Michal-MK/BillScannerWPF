40914da1a4b59865b9774d4d7a70510af41f1f3a
Michal-MK <michalhz159@gmail.com>
2019-02-21 20:20:39 +0100

    Added Extensions static class to extend standard types

---
+++ Core/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillScannerCore {
+	public static class Extensions {
+
+		public static string ReplaceAt(this string s, int index, char toPlace) {
+			StringBuilder sb = new StringBuilder(s);
+			sb[index] = toPlace;
+			return sb.ToString();
+		}
+	}
+}
